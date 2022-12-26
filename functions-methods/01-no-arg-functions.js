6c04f06337e7df166aed09c330e394133d8fa967
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-26 12:32:00 -0500

    functions and methods intro

---
+++ functions-methods/01-no-arg-functions.js
@@ -0,0 +1,18 @@
+function saveUser(email, password) {
+  const user = {
+    id: Math.random().toString(),
+    email: email,
+    password: password,
+  };
+
+  db.insert("users", user); // will not run
+}
+
+function saveUser(user) {
+  db.insert("users", user); // outsource certain functionality to different function
+}
+
+const user = new User("test@adsfsaf", "testers");
+user.save(); // create class and then method to save user (this would be the cleanest option)
+// note -- do not really need to refactor two argument function if order (of parameters) makes sense
+// clean code is about writing readable and meaningful code
